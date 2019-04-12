b3e089ffdcb330ba47663774aa953ad4924385ee
cppchedy <najjarchedy@gmail.com>
2019-04-12 21:48:45 +0100

    workaround

--- conanfile.py
+++ conanfile.py
@@ -13,6 +13,48 @@ class SeastarConan(ConanFile):
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake"
+    logworkaround = """
+#!/bin/bash
+# Abort on Error
+set -e
+
+export PING_SLEEP=30s
+export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export BUILD_OUTPUT=$WORKDIR/build.out
+
+touch $BUILD_OUTPUT
+
+dump_output() {
+   echo Tailing the last 500 lines of output:
+   tail -500 $BUILD_OUTPUT  
+}
+error_handler() {
+  echo ERROR: An error was encountered with the build.
+  dump_output
+  exit 1
+}
+# If an error occurs, run our error handler to output a tail of the build
+trap 'error_handler' ERR
+
+# Set up a repeating loop to send some output to Travis.
+
+bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
+PING_LOOP_PID=$!
+
+# My build is using maven, but you could build anything with this, E.g.
+# your_build_command_1 >> $BUILD_OUTPUT 2>&1
+# your_build_command_2 >> $BUILD_OUTPUT 2>&1
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:%s/seastar/build/_cooking/installed/lib
+./seastar/cooking.sh -- -DSeastar_DPDK=ON -DCMAKE_BUILD_TYPE=Release >> $BUILD_OUTPUT 2>&1
+cd seastar/build
+ninja >> $BUILD_OUTPUT 2>&1
+
+# The build finished without returning an error so dump a tail of the output
+dump_output
+
+# nicely terminate the ping output loop
+kill $PING_LOOP_PID
+"""
 
     def source(self):
         self.run("git clone --recursive https://github.com/scylladb/seastar.git")
@@ -25,10 +67,9 @@ class SeastarConan(ConanFile):
         #cmake.build()
 
         # Explicit way:
-        print(self.source_folder)
-        self.run('export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:%s/build/_cooking/installed/lib' 
-                    % self.source_folder)
-        self.run('./seastar/cooking.sh -- -DSeastar_DPDK=ON -DCMAKE_BUILD_TYPE=Release')
+        self.run("echo \"" +  logworkaround + "\" > seastar/build.sh" % self.build_folder)
+        self.run("chmod u+x seastar/build.sh")
+        self.run("./seastar/build.sh")
 
     def package(self):
         self.copy("*.h", dst="include", src="seastar")
