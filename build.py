4782f06682a5f34f089a9dba52b7f2b0358deb0f
cppchedy <najjarchedy@gmail.com>
2019-04-12 18:29:26 +0100

    forget sth

--- build.py
+++ build.py
@@ -3,6 +3,6 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     command = "sudo apt-get -qq update && sudo apt-get install -y stow"
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(docker_entry_script=command)
     builder.add_common_builds(shared_option_name="Seastar:shared")
     builder.run()
