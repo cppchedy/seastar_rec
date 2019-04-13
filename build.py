77cfcb2b6b0683d6a4a57b666b0faaa36904c4bf
cppchedy <najjarchedy@gmail.com>
2019-04-13 12:19:27 +0100

    change to right package

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    command = "sudo apt-get -qq update && sudo apt-get install -y stow xfsprogs"
+    command = "sudo apt-get -qq update && sudo apt-get install -y stow xfslibs-dev"
     builder = ConanMultiPackager(docker_entry_script=command)
     builder.add_common_builds(shared_option_name="Seastar:shared")
     builder.run()
