55a3ed52c6a791275bfdcd0184c3e00503ba5049
cppchedy <najjarchedy@gmail.com>
2019-04-13 01:30:50 +0100

    add xfs

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    command = "sudo apt-get -qq update && sudo apt-get install -y stow"
+    command = "sudo apt-get -qq update && sudo apt-get install -y stow xfsprogs"
     builder = ConanMultiPackager(docker_entry_script=command)
     builder.add_common_builds(shared_option_name="Seastar:shared")
     builder.run()
